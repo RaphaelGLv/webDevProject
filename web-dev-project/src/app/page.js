6e0cc7f3258363e49e11236071354df096ddd807
Raphael L <95318121+RaphaelGLv@users.noreply.github.com>
2023-10-30 18:36:10 -0300

    Corrigindo styles NavBar e LoginButton

--- web-dev-project/src/app/page.js
+++ web-dev-project/src/app/page.js
@@ -5,6 +5,8 @@ import { NavBar } from '@/components/NavBar/page'
 
 export default function Home() {
   return (
+    <>
       <NavBar />
+    </>
   )
 }
--- web-dev-project/src/components/LoginButton/page.js
+++ web-dev-project/src/components/LoginButton/page.js
@@ -2,11 +2,12 @@ import { Poppins } from "next/font/google";
 import * as S from "./styles";
 import Image from "next/image";
 import personIcon from "@/assets/svg/personIcon.svg"
+import { SlUser } from "react-icons/sl";
 
 export const LoginButton = () => {
     return (
         <S.Content onClick={console.log("url da pagina de login aqui!")}>
-            <Image id="personIcon" src={personIcon} alt=""></Image>
+            <SlUser id="personIcon" />
             <p>Entrar</p>
         </S.Content>
     )
