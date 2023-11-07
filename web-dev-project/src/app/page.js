c95c7e1eb4d1facc4e4414adb9500117b1417da1
Rennan Collado <rennancollado@gmail.com>
2023-11-07 15:18:49 -0300

    Feats: AnimalList

--- web-dev-project/src/app/page.js
+++ web-dev-project/src/app/page.js
@@ -2,11 +2,13 @@ import { Poppins } from 'next/font/google'
 import Image from 'next/image'
 import styles from './page.module.css'
 import { NavBar } from '@/components/NavBar/page'
+import { AnimalList } from '@/components/AnimalList/page'
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <AnimalList />
     </>
   )
 }
